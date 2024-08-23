af360ec364812f82326c9850263e828a2c0092ef
solenwa <solen.wanono@gmail.com>
2024-08-23 16:02:26 +0200

    Connecting db + adding api endpoints for products

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
-const db = require("./queries");
+const dbProducts = require("./database/productQueries");
 const port = 3000;
 var cors = require("cors");
 
@@ -20,13 +20,12 @@ app.get("/", (request, response) => {
   response.json({ info: "Node.js, Express, and Postgres API" });
 });
 
-/*
-app.get("/users", db.getUsers);
-app.get("/users/:id", db.getUserById);
-app.post("/users", db.createUser);
-app.put("/users/:id", db.updateUser);
-app.delete("/users/:id", db.deleteUser);
-*/
+// API Endpoints for wine products
+app.get("/products", dbProducts.getProducts);
+app.get("/products/:id", dbProducts.getProductById);
+app.post("/products", dbProducts.createProduct);
+app.put("/products/:id", dbProducts.updateProduct);
+app.delete("/products/:id", dbProducts.deleteProduct);
 
 app.listen(port, () => {
   console.log(`App running on port ${port}.`);
